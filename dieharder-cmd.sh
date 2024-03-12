dd7ea5ed05b1780be93ce583d1c3d37ebd67e795
krisjanisp <krisjanispetrucena@gmail.com>
2024-03-12 13:54:16 +0200

    script arguments

--- dieharder-cmd.sh
+++ dieharder-cmd.sh
@@ -1,3 +1,16 @@
 #! /usr/bin/env bash
 
-docker run -it $(docker build -q . -t rngeval)
+INPUT=qrng_raw_1e7.bin
+OUTPUT=qrng_raw_1e7.results.txt
+
+# Overwrite variables if arguments were provided
+if [ ! -z "$1" ]; then
+  INPUT="$1"
+fi
+
+if [ ! -z "$2" ]; then
+  OUTPUT="$2"
+fi
+
+IMAGE=$(docker build -q . -t rngeval)
+docker run > $OUTPUT  -v ./$INPUT:/$INPUT -it $IMAGE dieharder -a -f /$INPUT
